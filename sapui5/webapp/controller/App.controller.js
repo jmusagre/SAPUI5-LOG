e8e1cd360ebb3f69f26de2ef5a243440ab10b8ed
jmusagre <jmusagre@gmail.com>
2022-02-20 18:08:57 +0000

    05_Estructura_Proyecto_UI5

--- sapui5/webapp/controller/App.controller.js
+++ sapui5/webapp/controller/App.controller.js
@@ -1,28 +1,15 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
+    "sap/ui/core/mvc/Controller"
 ],
     /**
      * 
      * @param {typeof sap.ui.core.mvc.Controller} Controller 
-     * @param {typeof sap.m.MessageToast} MessageToast
-     * @param {typeof sap.ui.model.resource.ResourceModel} ResourceModel
      */
-    function (Controller, MessageToast, Models, ResourceModel) {
+    function (Controller) {
         "use strict";
         return Controller.extend("logaligroup.sapui5.controller.App", {
 
             onInit: function () {
-
-            },
-
-            onShowHello: function () {
-
-                var oBundle = this.getView().getModel("i18n").getResourceBundle();
-                var sRecipient = this.getView().getModel().getProperty("/recipient/name");
-                var sMsg = oBundle.getText("helloMsg", [sRecipient]);
-
-                MessageToast.show(sMsg);
 
             }
         });
