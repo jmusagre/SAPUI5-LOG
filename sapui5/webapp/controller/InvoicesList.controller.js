a86e6afb4238b9021f6aa39d59e6bd27408c4655
jmusagre <jmusagre@gmail.com>
2022-02-22 22:37:00 +0000

    05_Estructura_Proyecto_UI5

---
+++ sapui5/webapp/controller/InvoicesList.controller.js
@@ -0,0 +1,23 @@
+sap.ui.define([
+    'sap/ui/core/mvc/Controller',
+    'sap/ui/model/json/JSONModel'
+],
+/**
+ * 
+ * @param {typeof sap.ui.core.mvc.Controller} Controller 
+ * @param {typeof sap.ui.model.json.JSONModel} JSONModel
+ */
+    function (Controller, JSONModel) {
+        'use strict';
+        return Controller.extend("logaligroup.sapui5.controller.InvoicesList", {
+
+            onInit: function () {
+
+                var oViewModel = new JSONModel({
+                    usd: "USD",
+                    eur: "EUR"
+                });
+                this.getView().setModel(oViewModel, "currency");
+            }
+        });
+    });
