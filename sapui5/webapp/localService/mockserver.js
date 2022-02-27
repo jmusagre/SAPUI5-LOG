bcb710fe5097263d70a4acfaa2b0b905431e2f17
jmusagre <jmusagre@gmail.com>
2022-02-27 22:44:27 +0000

    05_Estructura_Proyecto_UI5

---
+++ sapui5/webapp/localService/mockserver.js
@@ -0,0 +1,91 @@
+sap.ui.define([
+    'sap/ui/core/util/MockServer',
+    'sap/ui/model/json/JSONModel',
+    'sap/base/util/UriParameters',
+    'sap/base/Log'
+],
+    /**
+     * 
+     * @param {sap.ui.core.util.MockServer} MockServer
+     * @param {sap.ui.model.json.JSONModel} JSONModel
+     * @param {sap.base.util.UriParameters} UriParameters
+     * @param {sap.base.Log} Log
+     */
+    function (MockServer, JSONModel, UriParameters, Log) {
+        'use strict';
+        var oMockServer,
+            _sAppPath = "logaligroup/sapui5/",
+            _sJsonFilesPath = _sAppPath + "localService/mockdata";
+
+        var oMockServerInterface = {
+
+            init: function (oOptionsParameter) {
+
+                var oOptions = oOptionsParameter || {};
+
+                return new Promise(function (fnResolve, fnReject) {
+
+                    var sManifestUrl = sap.ui.require.toUrl(_sAppPath + "manifest.json"),
+                        oManifestModel = new JSONModel(sManifestUrl);
+
+                    oManifestModel.attachRequestCompleted(function () {
+
+                        var oUriParameters = new UriParameters(window.location.href);
+                        var sJsonFilesUrl = sap.ui.require.toUrl(_sJsonFilesPath);
+                        var oMainDataSource = oManifestModel.getProperty("/sap.app/dataSources/mainService");
+                        var sMetadataUrl = sap.ui.require.toUrl(_sAppPath + oMainDataSource.settings.localUri);
+
+                        var sMockServerUrl = oMainDataSource.uri && new URI(oMainDataSource.uri).absoluteTo(sap.ui.require.toUrl(_sAppPath)).toString();
+
+                        if (!oMockServer) {
+                            oMockServer = new MockServer({
+                                rootUri: sMockServerUrl
+                            });
+                        } else {
+                            oMockServer.stop();
+                        }
+                        MockServer.config({
+                            autoRespond: true,
+                            autoRespondAfter: (oOptions.delay || oUriParameters.get("serverDelay") || 500)
+                        });
+                        oMockServer.simulate(sMetadataUrl, {
+                            sMockdataBaseUrl: sJsonFilesUrl,
+                            bGenerateMissingMockData: true
+                        });
+                        var aRequests = oMockServer.getRequests();
+                        var fnResponse = function (iErrCode, sMessage, aRequest) {
+                            aRequest.response = function (oXhr) {
+                                oXhr.respond(iErrCode, { "Content-Type": "text/plain;charset=utf-8", }, sMessage);
+                            };
+                        };
+                        if (oOptions.metadataError || oUriParameters.get("metadataError")) {
+                            aRequests.forEach(function (aEntry) {
+                                if (aEntry.path.toString().indexOf("$metadata") > -1) {
+                                    fnResponse(500, "Error metadata", aEntry);
+                                }
+                            });
+                        };
+                        var sErrorParam = oOptions.errorType || oUriParameters.get("errorType");
+                        var iErrorCode = sErrorParam === "badRequest" ? 400 : 500;
+
+                        if (sErrorParam) {
+                            aRequests.forEach(function (aEntry) {
+                                fnResponse(iErrorCode, sErrorParam, aEntry);
+                            });
+                        };
+                        oMockServer.setRequests(aRequests);
+                        oMockServer.start();
+                        Log.info("Running with mock data");
+                        fnResolve();
+                    });
+                    oManifestModel.attachRequestFailed(function () {
+                        var sError = "Failed to load the application manifest";
+                        Log.error(sError);
+                        fnReject(new Error(sError));
+                    });
+                });
+            }
+        };
+
+        return oMockServerInterface;
+    });
