5a20a1a200b724bb247690d186b9c92d0bfde551
jmusagre <jmusagre@gmail.com>
2022-04-03 13:04:58 +0000

    05_Estructura_Proyecto_UI5

--- sapui5/webapp/controller/Details.controller.js
+++ sapui5/webapp/controller/Details.controller.js
@@ -15,8 +15,9 @@ sap.ui.define([
 
             _onObjectMatch: function(oEvent) {
 
+                this.byId("rating").reset();
                 this.getView().bindElement({
-
+                   
                     path: "/" + window.decodeURIComponent(oEvent.getParameter("arguments").invoicePath),
                     model: "northwind"
                 });
@@ -42,6 +43,13 @@ sap.ui.define([
                     const oRouter = UIComponent.getRouterFor(this);
                     oRouter.navTo("RouteApp", {}, true);
                 }
+            },
+
+            onRatingChange: function (oEvent) {
+
+                const fValue = oEvent.getParameter("value");
+                const oResourceBundle = this.getView().getModel("i18n").getResourceBundle();
+                sap.m.MessageToast.show(oResourceBundle.getText("ratingConfirmation", [fValue]));
             }
         });
     });
